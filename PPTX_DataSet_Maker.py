882601e64cf6c0927da5b15bb23f4f71d276c739
Indian966 <rlaalsrb4175@gmail.com>
2021-01-06 01:17:26 +0900

    + 상위 경로를 제목으로 하는 슬라이드 추가 + 타겟 경로 입력하는 버튼으로 테스트 완료 + 로직 조정 완료

--- PPTX_DataSet_Maker.py
+++ PPTX_DataSet_Maker.py
@@ -17,35 +17,21 @@ import os
 
 
 class make_ppt() :
-    def test(self, directory):
+    def pre_view(self, directory):
         dir_list = os.listdir(directory)
         print(dir_list)
-        return dir_list
 
-
-    def ppt_work(self):
         prs = Presentation()
-        filelist = os.listdir('***')  # 읽어오고자 하는 폴더위치 삽입
-        print(filelist)
-
         blank_slide_layout = prs.slide_layouts[6]  # 이미지를 넣을 슬라이드 생성
-
         title_slide_layout = prs.slide_layouts[0]  # 텍스트를 넣을 슬라이드 생성
-
-        for slidetitle in filelist:  # 환자의 숫자만큼 반복
-            Coronal_img_temp = 'D:/김민규/최은정교수님/PPT_H' + '/' + slidetitle + '/c'
-            Transverse_img_temp = 'D:/김민규/최은정교수님/PPT_H' + '/' + slidetitle + '/t'
-
-            C_imglist = os.listdir(Coronal_img_temp)  # 이미지 파일 불러오기
-            T_imglist = os.listdir(Transverse_img_temp)
-
+        text_slide = prs.slides.add_slide(title_slide_layout)
+        text_slide.shapes.title.text = directory.split("\\")[-1]
+        for slidetitle in dir_list:  # case의 숫자만큼 반복
+            dir_path = directory + '/' + slidetitle
+            img_list = os.listdir(dir_path)  # 이미지 파일 불러오기
             text_slide = prs.slides.add_slide(title_slide_layout)
-            text_slide.shapes.title.text = slidetitle  # 환자 번호를 보여주는 슬라이드
-
-            text_slide = prs.slides.add_slide(title_slide_layout)
-            text_slide.shapes.title.text = 'Coronal'
-
-            for i in C_imglist:  # coronal 이미지 설정
+            text_slide.shapes.title.text = slidetitle  # case 번호를 보여주는 슬라이드
+            for image in img_list:  # 이미지 설정
 
                 left = Inches(1)
                 width = Inches(7.5)
@@ -53,21 +39,57 @@ class make_ppt() :
                 top = Inches(0)
 
                 image_slide = prs.slides.add_slide(blank_slide_layout)
-                img_path = Coronal_img_temp + '/' + i
+                img_path = dir_path + '/' + image
                 pic = image_slide.shapes.add_picture(img_path, left, top, width=width, height=height)
+        prs.save('DataSet.pptx')
+        return dir_list
+
+
+    def ppt_work(self, directory):
+        prs = Presentation()
+        dir_list = os.listdir(directory)  # 읽어오고자 하는 폴더위치 삽입
+        print(dir_list)
+
+        blank_slide_layout = prs.slide_layouts[6]  # 이미지를 넣을 슬라이드 생성
+
+        title_slide_layout = prs.slide_layouts[0]  # 텍스트를 넣을 슬라이드 생성
+
+        for slidetitle in dir_list:  # case의 숫자만큼 반복
+            dir_path = directory + '/' + slidetitle
+            # Transverse_img_temp = 'D:/김민규/최은정교수님/PPT_H' + '/' + slidetitle + '/t'
+
+            img_list = os.listdir(dir_path)  # 이미지 파일 불러오기
+            # T_imglist = os.listdir(Transverse_img_temp)
 
             text_slide = prs.slides.add_slide(title_slide_layout)
-            text_slide.shapes.title.text = 'Transverse'
+            text_slide.shapes.title.text = slidetitle  # case 번호를 보여주는 슬라이드
 
-            for i in T_imglist:  # transverse 이미지 설정
+            # text_slide = prs.slides.add_slide(title_slide_layout)
+            # text_slide.shapes.title.text = 'Coronal'
 
-                left = Inches(0)
-                top = Inches(1.5)
-                width = Inches(10)
-                height = Inches(5)
+            for image in img_list:  # coronal 이미지 설정
+
+                left = Inches(1)
+                width = Inches(7.5)
+                height = Inches(7.5)
+                top = Inches(0)
 
                 image_slide = prs.slides.add_slide(blank_slide_layout)
-                img_path = Transverse_img_temp + '/' + i
+                img_path = dir_path + '/' + image
                 pic = image_slide.shapes.add_picture(img_path, left, top, width=width, height=height)
 
-        prs.save('DataSet.pptx')  # 파일 이름 (확장자 반드시 포함), D:\김민규\Python 이 경로로 저장됨
+            # text_slide = prs.slides.add_slide(title_slide_layout)
+            # text_slide.shapes.title.text = 'Transverse'
+            #
+            # for i in T_imglist:  # transverse 이미지 설정
+            #
+            #     left = Inches(0)
+            #     top = Inches(1.5)
+            #     width = Inches(10)
+            #     height = Inches(5)
+            #
+            #     image_slide = prs.slides.add_slide(blank_slide_layout)
+            #     img_path = Transverse_img_temp + '/' + i
+            #     pic = image_slide.shapes.add_picture(img_path, left, top, width=width, height=height)
+
+        prs.save('DataSet1.pptx')  # 파일 이름 (확장자 반드시 포함), D:\김민규\Python 이 경로로 저장됨
--- main.py
+++ main.py
@@ -11,26 +11,31 @@ class make_gui(QWidget):
         self.initUI()
 
     def initUI(self):
+        # 버튼
+        self.dir_return = QPushButton('Return', self)
+        self.dir_return.clicked.connect(self.text_changed)
+        self.save_return = QPushButton('Return', self)
+        self.dir_return.clicked.connect(self.ppt_work) ## 피피티 만드는 함수 호출
 
-        btn = QPushButton('Return', self)
-        btn.move(50, 50)
-        btn.resize(btn.sizeHint())
-        # btn.clicked.connect()
-
-        self.lbl1 = QLabel('Enter your sentence:')
-        self.te = QTextEdit()
-        self.te.setAcceptRichText(False)
-        self.lbl2 = QLabel()
-        # text = '\n'.join(f.test())
-        # self.lbl2.setText(text)
+        # 레이블, 텍스트박스
+        self.lbl1 = QLabel('Enter target directory :')
+        self.sub_dir_lbl = QLabel()
+        self.save_lbl = QLabel('Enter save directory :')
+        self.dir_input_te = QTextEdit()
+        self.dir_input_te.setAcceptRichText(False)
+        self.save_dir_te = QTextEdit()
+        self.save_dir_te.setAcceptRichText(False)
 
 
-        self.te.textChanged.connect(self.text_changed)
-
+        # 버티컬 레이아웃
         vbox = QVBoxLayout()
         vbox.addWidget(self.lbl1)
-        vbox.addWidget(self.te)
-        vbox.addWidget(self.lbl2)
+        vbox.addWidget(self.dir_input_te)
+        vbox.addWidget(self.dir_return)
+        vbox.addWidget(self.sub_dir_lbl)
+        vbox.addWidget(self.save_lbl)
+        vbox.addWidget(self.save_dir_te)
+        vbox.addWidget(self.save_return)
         vbox.addStretch()
 
         self.setLayout(vbox)
@@ -40,16 +45,18 @@ class make_gui(QWidget):
         self.show()
 
     def text_changed(self):
-        directory = self.te.toPlainText()
+        directory = self.dir_input_te.toPlainText() # 입력한 경로
         f = make_ppt()
-        text = '\n'.join(f.test(directory))
-        self.lbl2.setText('The target directory is \n' + text)
+        text = '\n'.join(f.pre_view(directory))
+        self.sub_dir_lbl.setText('The sub directory is :\n' + text)
 
-    def center(self): # 가운데 정렬
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+    def ppt_work(self):
+        return "yay!"
+    # def center(self): # 가운데 정렬
+    #     qr = self.frameGeometry()
+    #     cp = QDesktopWidget().availableGeometry().center()
+    #     qr.moveCenter(cp)
+    #     self.move(qr.topLeft())
 
 
 
