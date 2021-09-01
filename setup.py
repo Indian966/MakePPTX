781c433406c7d39a6bc31afc029d88e6ba96ae63
Indian966 <rlaalsrb4175@gmail.com>
2021-09-01 16:59:21 +0900

    + setup.py update

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Indian966/MakePPTX",
-    packages=setuptools.find_packages(),
+    packages= [
+        'PPTX_DataSet_Maker.py',
+        'PPTX_DataSet_Maker_main.py'
+        ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
