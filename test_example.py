6db22f1344ee50d00820b914a5ff2789e9a7be26
Indian966 <rlaalsrb4175@gmail.com>
2021-09-06 23:53:49 +0900

    + test_example.py update

--- test_example.py
+++ test_example.py
@@ -1,6 +1,10 @@
 import sys
 sys.path.append("D:\Python\MakePPTX\code")
 # 절대경로가 아닌 상대경로가 필요함
+from .code import PPTX_DataSet_Maker
 
 def test_example():
   assert 1
+
+def pre_view(self,target_dir) :
+  assert len(pre_view("C:\Users\user\Downloads\Test")) > 1
