15909200099a36c903454bc99fcd16f76657e47b
Indian966 <rlaalsrb4175@gmail.com>
2021-09-13 20:57:20 +0900

    + create directory for test code

---
+++ test_code/test_dataset_maker.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+    def test_dataset(self):
+        self.assertEqual()
+
+if __name__ == '__main__':
+    unittest.main()
